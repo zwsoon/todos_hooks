9bb7f43980249f9e4f2770ec09fd9a90d9922423
Zachary Soon <zwsoon@gmail.com>
2020-02-11 00:55:56 -0500

    adding complete button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, {useState} from 'react'
 import './App.css';
 
-function Todo({todo, index}){
+function Todo({todo, index, completeTodo}){
   return(
-    <div className="todo">
+    <div style={{textDecoration: todo.isComplete ? 'line-through':''}} className="todo">
       {todo.text}
+      <div>
+        <button onClick={()=>completeTodo(index)} >Complete</button>
+      </div>
     </div>
   )
 };
+
+// addTodo is being called by destructing from props
+function TodoForm({addTodo}){
+  const [value, setValue] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+  return(
+    <form onSubmit={handleSubmit}>
+      <input 
+      type="text" 
+      className="input" 
+      value={value} 
+      placeholder="Add Todo..."
+      onChange={e=>setValue(e.target.value)}/> 
+    </form>
+  )
+}
 
 function App(){
   const [todos, setTodos] = useState(
@@ -27,12 +51,24 @@ function App(){
     ]
   );
 
+  const addTodo = text =>{
+    const newTodos = [...todos, {text}];
+    setTodos(newTodos);
+  }
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  }
+
   return(
     <div className="app">
      <div className="todo-list">
         {todos.map((todo,index)=>(
-          <Todo key={index} index={index} todo={todo}/>
+          <Todo key={index} index={index} todo={todo} completeTodo={completeTodo}/>
         ))}
+        <TodoForm addTodo={addTodo} />
      </div>
     </div>
   )
